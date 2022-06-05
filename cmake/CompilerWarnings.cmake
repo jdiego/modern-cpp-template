4b869967b27022cc92844fa135852071264d35d1
Diego Saraiva <diegosaraiva@gmail.com>
2022-06-05 08:28:06 -0300

    add WarningsAsErrors cmake module as a option

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -50,6 +50,9 @@ function(set_project_warnings project_name)
     if(${PROJECT_NAME_UPPERCASE}_WARNINGS_AS_ERRORS)
         set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
         set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
+        if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_COMPILER_VERSION GREATER_EQUAL 10.0)
+            target_compile_options(${PROJECT_NAME} PRIVATE -Wdeprecated-copy-dtor -Wnewline-eof)
+        endif()
     endif()
 
     set(GCC_WARNINGS

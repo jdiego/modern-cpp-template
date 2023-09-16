be19fa861a03d8e4bb6601153f18134d518bf852
Diego Saraiva <diegosaraiva@gmail.com>
2023-09-16 14:57:33 -0300

    Update github actions

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -2,33 +2,40 @@ set(CPM_DOWNLOAD_VERSION 0.38.2)
 set(CPM_HASH_SUM "bd6d4604c8154ee2a298187d6b47b850b913d90a3c26b63beb60677332df0c85")
 
 if(CPM_SOURCE_CACHE)
-  set(CPM_DOWNLOAD_LOCATION "${CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+    set(CPM_DOWNLOAD_LOCATION "${CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
 elseif(DEFINED ENV{CPM_SOURCE_CACHE})
-  set(CPM_DOWNLOAD_LOCATION "$ENV{CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+    set(CPM_DOWNLOAD_LOCATION "$ENV{CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
 else()
-  set(CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+    set(CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
 endif()
 
 # Expand relative path. This is important if the provided path contains a tilde (~)
 get_filename_component(CPM_DOWNLOAD_LOCATION ${CPM_DOWNLOAD_LOCATION} ABSOLUTE)
 
 function(download_cpm)
-  message(STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
-  file(DOWNLOAD
+    message(STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
+    file(DOWNLOAD
        https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
-       ${CPM_DOWNLOAD_LOCATION}
-  )
+       ${CPM_DOWNLOAD_LOCATION} STATUS CPM_DOWNLOAD_STATUS
+    )
+    list(GET CPM_DOWNLOAD_STATUS 0 CPM_DOWNLOAD_STATUS_CODE)
+    list(GET CPM_DOWNLOAD_STATUS 1 CPM_DOWNLOAD_ERROR_MESSAGE)
+    if(${CPM_DOWNLOAD_STATUS_CODE} EQUAL 0)
+        message(STATUS "CPM: Download completed successfully.")
+    else()
+        message(FATAL_ERROR "CPM: Error occurred during download: ${CPM_DOWNLOAD_ERROR_MESSAGE}")
+    endif()
 endfunction()
 
 if(NOT (EXISTS ${CPM_DOWNLOAD_LOCATION}))
-  download_cpm()
-else()
-  # resume download if it previously failed
-  file(SHA256 ${CPM_DOWNLOAD_LOCATION} CPM_CHECK)
-  if(NOT "${CPM_CHECK}" STREQUAL CPM_HASH_SUM)
     download_cpm()
-  endif()
-  unset(check)
+else()
+    # resume download if it previously failed
+    file(SHA256 ${CPM_DOWNLOAD_LOCATION} CPM_CHECK)
+    if(NOT "${CPM_CHECK}" STREQUAL CPM_HASH_SUM)
+        download_cpm()
+    endif()
+    unset(check)
 endif()
 
 include(${CPM_DOWNLOAD_LOCATION})

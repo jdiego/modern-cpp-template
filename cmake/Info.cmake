3a202d23593b652c94e1dfeb4d08c845d5fffe96
Diego Saraiva <diegosaraiva@gmail.com>
2023-09-16 15:46:12 -0300

    renamming....

--- cmake/Info.cmake
+++ cmake/Info.cmake
@@ -2,5 +2,5 @@
 set(MAIN_PROJECT_NAME modern_cpp_project)
 set(MAIN_PROJECT_DESCRIPTION "ModernCppProject is a project template for Modern C++")
 set(MAIN_CXX_VERSION 23)
-set(STANDALONE_PROJECT_NAME App)
+set(STANDALONE_PROJECT_NAME Standalone)
 set(TEST_PROJECT_NAME TestRunner)

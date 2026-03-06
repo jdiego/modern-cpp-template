7405414ad618c1e757d6f34256977103a545db87
Diego Saraiva <diegosaraiva@gmail.com>
2026-03-06 08:09:35 -0300

    improvements on cmake

--- src/greeter.cpp
+++ src/greeter.cpp
@@ -1,7 +1,8 @@
 #include "modern_cpp_project/greeter.hpp"
+
 #include <fmt/format.h>
 #include <spdlog/spdlog.h>
-
+#include <vector>
 using namespace greeter;
 
 Greeter::Greeter(std::string _name) : name(std::move(_name)) {
@@ -9,18 +10,20 @@ Greeter::Greeter(std::string _name) : name(std::move(_name)) {
 
 std::string Greeter::greet(LanguageCode lang) const {
     spdlog::info("Welcome to spdlog!");
+    std::vector<int> str;
+
     switch (lang) {
-        case LanguageCode::EN:
-            return fmt::format("Hello, {}!", name);
-        case LanguageCode::DE:
-            return fmt::format("Hallo {}!", name);
-        case LanguageCode::ES:
-            return fmt::format("¡Hola {}!", name);
-        case LanguageCode::FR:
-            return fmt::format("Bonjour {}!", name);
-        default:
-           std::string msg = fmt::format("Error: The Language Code is invalid: {}", name);
-           spdlog::error(msg);
-           return msg;
+    case LanguageCode::EN:
+        return fmt::format("Hello, {}!", name);
+    case LanguageCode::DE:
+        return fmt::format("Hallo {}!", name);
+    case LanguageCode::ES:
+        return fmt::format("¡Hola {}!", name);
+    case LanguageCode::FR:
+        return fmt::format("Bonjour {}!", name);
+    default:
+        std::string msg = fmt::format("Error: The Language Code is invalid: {}", name);
+        spdlog::error(msg);
+        return msg;
     }
 }
